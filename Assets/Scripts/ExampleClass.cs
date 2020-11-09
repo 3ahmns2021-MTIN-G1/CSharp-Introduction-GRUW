b0664315dd19d2fb7cafcaeb57a819c3b56f4147
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2020-11-09 14:13:14 +0100

    Add ExampleClass : Deklaration einer Klasse

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,17 @@
+﻿// Bibliotheken / Asembly Referenz / Namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Deklaration einer Klasse
+
+// public           -> access modifier (Zugriffsrechte) | keyword (vorreservierte Schlüsselwort)
+// class            -> keyword zur Deklaration einer Klasse
+// ExampleClass     -> identifier (Name der Klasse)
+// : MonoBehaviour  -> Erstellen eine Unterklasse von MonoBehaviour
+// {}               -> Scope/Wirkungsbereich (Was ist alles in der Klasse enthalten)
+public class ExampleClass : MonoBehaviour
+{
+
+}
