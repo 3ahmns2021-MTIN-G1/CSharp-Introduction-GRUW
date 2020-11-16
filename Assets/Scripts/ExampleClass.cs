16be24b80929d98dcf1e794d7ac8042edc62c0dc
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2020-11-16 12:08:40 +0100

    Add Declaration of variables

--- Assets/Scripts/ExampleClass.cs
+++ Assets/Scripts/ExampleClass.cs
@@ -13,5 +13,23 @@ using UnityEngine;
 // {}               -> Scope/Wirkungsbereich (Was ist alles in der Klasse enthalten)
 public class ExampleClass : MonoBehaviour
 {
+    // Deklaration von Variablen
+    // Variablen sind Container für Value (Werte) oder References (Verweise)
 
+    // Variablentypen mit Werten
+    // public           -> access modifier (Wer darf von außen auf die Variable zugreifen)
+    // int              -> Variablentyp (In diesem Fall Integer - Ganze Zahl)
+    // ganzeZahlVariable-> identifier (Name der Variable)
+    // ;                -> Ende des Statements (Deklaration der Variable)
+
+    public int ganzeZahlVariable;  // Ganze Zahlen - Integer - Instanzvariable (innerhalb der ganzen Klasse gültig)
+    // Gleitkommazahlen
+    // Text
+    // Binäre Entscheidung (Boolscher Wert)
+
+
+    public void TestFunktion()
+    {
+        // Variable die NUR innerhalb der Funktion gültig ist
+    }
 }
